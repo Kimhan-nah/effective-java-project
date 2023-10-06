c1abda00b4e7a1b2efe0194ffb8732bcb0f20f67
Kimhannah <skqkdldhf98@naver.com>
2023-10-06 23:09:10 +0900

    :hammer: refactor: Add findAll function to Repository Interface

--- src/main/java/repository/RecordRepository.java
+++ src/main/java/repository/RecordRepository.java
@@ -1,13 +1,18 @@
 package repository;
 
 import model.Record;
+import model.User;
 
 import java.util.List;
 import java.util.Optional;
 
+// TODO Transaction 확인 필요
 public interface RecordRepository {
   Record save(Record record);
   Optional<Record> findById(Long id);
-  Optional<Record> findByName(String name);
   List<Record> findAll();
+  List<Record> findAllById(Long id);
+  List<Record> findAllByUser(User user);
+  List<Record> findAllByType(String type);
+  // TODO delete, update
 }
--- src/main/java/repository/UserRepository.java
+++ src/main/java/repository/UserRepository.java
@@ -5,10 +5,11 @@ import model.User;
 import java.util.List;
 import java.util.Optional;
 
+// TODO Transaction 확인 필요
 public interface UserRepository {
   User save(User user);
   Optional<User> findById(Long id);
-  Optional<User> findByName(String name);
+  Optional<User> findByNickname(String nickname);
   List<User> findAll();
-
+  // TODO delete, update
 }
